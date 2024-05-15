d0f6b5dd6d0a7c1ad21e0c23fc42b5696debe237
jose <jgabriel2608@gmail>
2024-05-15 16:08:38 -0500

    trabajo en clase

--- EjemplosArreglos/EjercicioArreglosPOO/src/paquete02/Ejecutor2.java
+++ EjemplosArreglos/EjercicioArreglosPOO/src/paquete02/Ejecutor2.java
@@ -16,7 +16,16 @@ public class Ejecutor2 {
          LibretaCalificacion libreta2 = new LibretaCalificacion(nombreEstudiantes,
                 notas);
          libreta2.establecerPromedio();
+         /*
+         si es que esta linea no la pongo nose va a presentar ningun valor y se
+         va tomar un valor por defecto que seria null 
+         */
          libreta2.establecerPromedioCualitativo();
+         /*
+         si es que esta linea en este program no se la pone al rato de correr el
+         programa no se va apresentar ningun promedio culitattivo y se va dar uno
+         por defecto que seria el null
+         */
          System.out.printf("%s",libreta2);
     }
 }
